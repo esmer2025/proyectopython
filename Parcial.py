78223454bec0ee583376656938a123a0fb01587f
esmeralda <ixchelsic@gmail.com>
2026-09-18 16:37:03 -0600

    Un cambio, se le agrego un print

--- Parcial.py
+++ Parcial.py
@@ -1,7 +1,8 @@
 nombre = "Esmeralda"
 edad = 17
-curso = "Electrónica"
+curso = "Programacion"
 
 print("Nombre:", nombre)
 print("Edad:", edad)
 print("Curso:", curso)
+print("Su color favorito es el morado jajajaja")
